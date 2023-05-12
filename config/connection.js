17e97b23d7c7881f0ffd182cc1d29fa12e476b84
cristino4 <117871733+cristino4@users.noreply.github.com>
2023-05-11 21:30:58 -0700

    comments are not available when logged off

--- config/connection.js
+++ config/connection.js
@@ -2,20 +2,22 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  const sequelize = new Sequelize(process.env.JAWSDB_URL);
+  module.exports = sequelize;
 } else {
+    const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+  module.exports = sequelize;
+};
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-)};
 
-module.exports = sequelize;
 
 
